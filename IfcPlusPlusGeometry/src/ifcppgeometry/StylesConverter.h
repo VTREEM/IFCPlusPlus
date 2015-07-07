3593f9a0459a3441ac73144ea509f15f59a6d63e
Sylvain MARIE <sylvain.marie@vtreem.com>
2015-07-07 19:24:34 +0200

    [Fix] Using (0.2,0.2,0.2) for the default ambient color.

--- IfcPlusPlusGeometry/src/ifcppgeometry/StylesConverter.h
+++ IfcPlusPlusGeometry/src/ifcppgeometry/StylesConverter.h
@@ -221,7 +221,7 @@ public:
 					convertIfcColourRgb( surf_color, surface_color );
 				}
 
-				carve::geom::vector<4> ambient_color = carve::geom::VECTOR( 0.0, 0.0, 0.0, 1.f );
+				carve::geom::vector<4> ambient_color = carve::geom::VECTOR( 0.2, 0.2, 0.2, 1.f );
 				//carve::geom::vector<4> emissive_color( 0.0f, 0.0f, 0.0f, 1.f );
 				carve::geom::vector<4> diffuse_color( surface_color );
 				carve::geom::vector<4> specular_color( ambiant_color );
